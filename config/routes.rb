a0472d76ba886301f86ee324e882d1eb49c394c8
hengxinchen <chenhengxin@gmail.com>
2017-03-25 22:58:09 +0800

    miplement my groups

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
    resources :groups do
               member do
@@ -9,5 +10,8 @@ Rails.application.routes.draw do
 
    resources :posts
  end
+        namespace :account do
+        resources :groups
+end
  root 'groups#index'
  end
